01f6cbece9e86702c8257c740bef0d85dfae5b3e
Quy Dinh <quy.dinh3195@gmail.com>
2024-07-16 15:03:04 +0700

    ICE-28 Test Jaeger docker (WIP)
    - Issue: duplicate spans

--- models/reviews-parsing-ner-aspects/model.py
+++ models/reviews-parsing-ner-aspects/model.py
@@ -5,6 +5,37 @@ from mlserver.codecs import StringCodec
 from mlserver.types import InferenceRequest, InferenceResponse, ResponseOutput
 import mlflow
 
+from opentelemetry import trace
+from opentelemetry.exporter.jaeger.thrift import JaegerExporter
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+
+trace.set_tracer_provider(
+    TracerProvider(
+        resource=Resource.create({SERVICE_NAME: "reviews-parsing-ner-aspects-mlserver"})
+    )
+)
+tracer = trace.get_tracer(__name__)
+
+# create a JaegerExporter
+jaeger_exporter = JaegerExporter(
+    # configure agent
+    agent_host_name="jaeger",
+    agent_port=6831,
+    # optional: configure also collector
+    # collector_endpoint='http://localhost:14268/api/traces?format=jaeger.thrift',
+    # username=xxxx, # optional
+    # password=xxxx, # optional
+    # max_tag_value_length=None # optional
+)
+
+# Create a BatchSpanProcessor and add the exporter to it
+span_processor = BatchSpanProcessor(jaeger_exporter)
+
+# Add to the tracer
+trace.get_tracer_provider().add_span_processor(span_processor)
+
 
 class CustomTransformersModel(MLModel):
     async def load(self) -> bool:
@@ -16,31 +47,36 @@ class CustomTransformersModel(MLModel):
         return self.ready
 
     async def predict(self, payload: InferenceRequest) -> InferenceResponse:
-        # Decode the input text
-        logger.info(f"{payload.inputs=}")
-        input_texts = StringCodec.decode_input(payload.inputs[0])
-        logger.info(f"{input_texts=}")
+        with tracer.start_as_current_span("make-prediction"):
+            with tracer.start_as_current_span("parse-input") as span:
+                # Decode the input text
+                logger.info(f"{payload.inputs=}")
+                span.set_attribute("payload.inputs.shape", payload.inputs[0].shape)
+                input_texts = StringCodec.decode_input(payload.inputs[0])
+                logger.info(f"{input_texts=}")
 
-        # Make predictions
-        predictions = self.model(input_texts)
-        logger.info(f"{predictions=}")
+                # Make predictions
+            with tracer.start_as_current_span("predict") as span:
+                predictions = self.model(input_texts)
+                logger.info(f"{predictions=}")
 
-        for prediction in predictions:
-            for item in prediction:
-                item["score"] = float(item["score"])
-        response_bytes = json.dumps(predictions).encode("UTF-8")
+                for prediction in predictions:
+                    for item in prediction:
+                        item["score"] = float(item["score"])
+                response_bytes = json.dumps(predictions).encode("UTF-8")
 
-        # Prepare the response
-        response = ResponseOutput(
-            name="predictions",
-            shape=[len(predictions)],
-            datatype="BYTES",
-            data=response_bytes,
-        )
+            with tracer.start_as_current_span("format-output") as span:
+                # Prepare the response
+                response = ResponseOutput(
+                    name="predictions",
+                    shape=[len(predictions)],
+                    datatype="BYTES",
+                    data=response_bytes,
+                )
 
-        return InferenceResponse(
-            model_name=self.name,
-            model_version=self.version,
-            outputs=[response],
-            parameters=types.Parameters(content_type="application/json"),
-        )
+                return InferenceResponse(
+                    model_name=self.name,
+                    model_version=self.version,
+                    outputs=[response],
+                    parameters=types.Parameters(content_type="application/json"),
+                )
